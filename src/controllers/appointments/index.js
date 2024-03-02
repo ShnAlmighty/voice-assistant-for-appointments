8acecf64c6d79a81d3016e902eefcf9492f8c630
Shantanu Patnaik <patnaikshantanu@gmail.com>
2024-03-02 13:08:27 +0530

    added routes & con'lers for managing ap'ments through call

--- src/controllers/appointments/index.js
+++ src/controllers/appointments/index.js
@@ -1,4 +1,9 @@
+const twilio = require('twilio');
+const moment = require('moment');
+const VoiceResponse = twilio.twiml.VoiceResponse;
+
 const Appointment = require('../../models/appointment');
+const { extractDateTime, reduceConfirmation } = require('../utils/speech');
 
 const createAppointment = async(req, res) => {
   try {
@@ -13,9 +18,42 @@ const createAppointment = async(req, res) => {
     }
     const appointment = new Appointment(appointmentObj);
     await appointment.save();
-    res.status(201).json({ message: 'Appointment scheduled successfully', appointment });
+    res.status(201).json({ message: 'Appointment Created successfully', appointment });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to schedule appointment', message: error.message });
+    res.status(500).json({ error: 'Failed to Create the appointment', message: error.message });
+  }
+};
+
+const schedulePatientAppointment = async(req, res) => {
+  const twiml = new VoiceResponse();
+  const userInput = req.body.SpeechResult;
+  try {
+    const { date_utc } = extractDateTime(userInput);
+    const appointment = await Appointment.findOne({appointmentTime: date_utc });
+    if(!appointment){
+      twiml.say('Appointment at the given time is not found, kindly try again.');
+      twiml.redirect('/voicecalls/menu');
+    } else {
+      appointment.patientContact = req.body.From;
+      appointment.status = 'scheduled';
+      await appointment.save();
+
+      const formattedDay = moment(appointment.appointmentTime).format('dddd');
+      const formattedDate = moment(appointment.appointmentTime).format('MMMM Do');
+      const formattedTime = moment(appointment.appointmentTime).format('h:mm a');
+
+      const str = `Your appointment with ${appointment.doctorName} is booked on ${formattedDay}, ${formattedDate}, at ${formattedTime}`;
+      twiml.say(str);
+      twiml.redirect('/voicecalls/menu');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  } catch (error) {
+    console.log("Error in scheduling patient's appointment: ", error);
+    twiml.say('Appologies, I am not able to schedule an appointment due to some issues. Kindly try again later.');
+    twiml.redirect('/voicecalls/menu');
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
   }
 };
 
@@ -27,7 +65,8 @@ const readAppointment = async (req, res) => {
     }
     res.json({ appointment });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to cancel appointment', message: error.message });
+    console.log("Error in reading appointment: ", error);
+    res.status(500).json({ error: 'Failed to read appointment', message: error.message });
   }
 };
 
@@ -40,7 +79,8 @@ const readAllAppointments = async (req, res) => {
     }
     res.json({ appointments });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to cancel appointment', message: error.message });
+    console.log("Error in reading all appointments: ", error);
+    res.status(500).json({ error: 'Failed to read all appointments', message: error.message });
   }
 };
 
@@ -54,7 +94,42 @@ const deleteAppointment = async (req, res) => {
     await appointment.save();
     res.json({ message: 'Appointment cancelled successfully', appointment });
   } catch (error) {
+    console.log("Error in deleting appointment: ", error);
     res.status(500).json({ error: 'Failed to cancel appointment', message: error.message });
+  }
+};
+
+const cancelPatientAppointment = async(req, res) => {
+  const twiml = new VoiceResponse();
+  try {
+    const patientContact = req.body.From; //TODO: use xss to parse this first
+    const userInput = req.body.SpeechResult;
+    const choice = reduceConfirmation(userInput);
+    console.log({userInput, choice})
+
+    if (choice == "Yes") {
+      const appointment = await Appointment.findOne({ patientContact, status: 'scheduled' });
+      appointment.patientContact = undefined;
+      appointment.status = 'available';
+      await appointment.save();
+      twiml.say('Your appointment with ' + appointment.doctorName + ' on ' + moment(appointment.appointmentTime).format('MMMM Do, YYYY, h:mm a') + ' is cancelled.');
+      twiml.redirect('/voicecalls/menu');
+    } else if(choice == "No") {
+      twiml.say('Okay, moving back to main menu.');
+      twiml.redirect('/voicecalls/menu');
+    } else {
+      twiml.say('I did not understand that. Please try again later.');
+      twiml.redirect('/voicecalls/menu');
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  } catch (error) {
+    console.log("Error in cancelling patient's appointment: ", error);
+    twiml.say('Appologies, I am not able to cancel the appointment due to some issues. Kindly try again later.');
+    twiml.redirect('/voicecalls/menu');
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
   }
 };
 
@@ -68,14 +143,68 @@ const updateAppointment = async (req, res) => {
     }
     res.json({ message: 'Appointment rescheduled successfully', appointment });
   } catch (error) {
+    console.log("Error in updating ppointment: ", error);
     res.status(500).json({ error: 'Failed to reschedule appointment', message: error.message });
   }
-}
+};
+
+const reschedulePatientAppointment = async(req, res) => {
+  const twiml = new VoiceResponse();
+  try {
+    const patientContact = req.body.From; //TODO: use xss to parse this first
+    const userInput = req.body.SpeechResult;
+    const choice = reduceConfirmation(userInput);
+    console.log({userInput, choice});
+    if (choice == "Yes") {
+      twiml.say('I will be glad to help you in booking another appointment. Let me check the available appointment slots.');
+      const availableSlots = await Appointment.find({status: 'available'}).sort('appointmentTime').limit(10).lean();
+      if (availableSlots.length === 0) {
+        twiml.say('Sorry, there are no appointment slots available currently. Kindly try again later.');
+        twiml.redirect('/voicecalls/menu');
+      } else {
+        // Make the previous appointment available for others
+        await Appointment.findOneAndUpdate({ patientContact }, { $unset: { patientContact: "" }, status: 'available' });
+        
+        twiml.say('Here are the available appointment slots, kindly say the day and time of the appointment you want to book. For example, you can say I want to book the slot at Friday 11 am.');
+        twiml.say('Available appointment slots are');
+        for (let i = 0; i < availableSlots.length; i++) {
+          const slot = availableSlots[i];
+          const formattedTime = moment(slot.appointmentTime).format('MMMM Do, YYYY, h:mm a');
+          twiml.say(`${i + 1}. ${formattedTime} with ${slot.doctorName}`);
+        }
+        const gather = twiml.gather({
+          input: 'speech',
+          action: '/appointments/schedule',
+          method: 'POST',
+          speechTimeout: 5
+        });
+        gather.say('You can say the desired appointment day followed by the time now');
+      }
+    } else if(choice == "No") {
+      twiml.say('Okay, moving back to main menu.');
+      twiml.redirect('/voicecalls/menu');
+    } else {
+      twiml.say('I did not understand that. Please try again later.');
+      twiml.redirect('/voicecalls/menu');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  } catch (error) {
+    console.log("Error in rescheduling patient's appointment: ", error);
+    twiml.say('Appologies, I am not able to reschedule the appointment due to some issues. Kindly try again later.');
+    twiml.redirect('/voicecalls/menu');
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  }
+};
 
 module.exports = {
   createAppointment,
+  schedulePatientAppointment,
   readAppointment,
   readAllAppointments,
   deleteAppointment,
-  updateAppointment
+  cancelPatientAppointment,
+  updateAppointment,
+  reschedulePatientAppointment
 }
--- src/routes/appointments/index.js
+++ src/routes/appointments/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../../controllers/appointments');
 
-// Schedule an appointment
+// Create an appointment
 router.post('/', appointmentController.createAppointment);
+
+// Schedule a patient's appointment
+router.post('/schedule', appointmentController.schedulePatientAppointment);
 
 // Read all appointments
 router.get('/all', appointmentController.readAllAppointments);
@@ -11,8 +14,14 @@ router.get('/all', appointmentController.readAllAppointments);
 // Read a single appointment
 router.get('/:id', appointmentController.readAppointment);
 
+// Cancel a patient's appointment
+router.delete('/schedule', appointmentController.cancelPatientAppointment);
+
 // Cancel an appointment
 router.delete('/:id', appointmentController.deleteAppointment);
+
+// Reschedule a patient's appointment
+router.patch('/schedule', appointmentController.reschedulePatientAppointment);
 
 // Reschedule an appointment
 router.patch('/:id', appointmentController.updateAppointment);
